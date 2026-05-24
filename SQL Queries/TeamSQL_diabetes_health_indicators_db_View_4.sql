28317976a45654fa2b22554e56c0d959753fd0be
Tayyab Fiaz <147021539+tayyabmfiaz@users.noreply.github.com>
2026-05-23 21:23:07 -0400

    Delete SQL Queries directory

--- SQL Queries/TeamSQL_diabetes_health_indicators_db_View_4.sql
+++
@@ -1,8 +0,0 @@
-USE diabetes_health_indicators_db;
-DROP VIEW IF EXISTS smoker_diabetes_distribution;
-CREATE VIEW smoker_diabetes_distribution AS
-SELECT COUNT(Patient_id) AS total_smokers, SUM(Diabetes = 1) AS prediabetic_smoker, SUM(Diabetes = 2) AS diabetic_smoker
-FROM Patient
-JOIN Health_Behaviors USING (Health_Behavior_id)
-WHERE Health_Behaviors.Smoker = 1
-  AND (Patient.Diabetes = 0 OR Patient.Diabetes = 1 OR Patient.Diabetes = 2);
